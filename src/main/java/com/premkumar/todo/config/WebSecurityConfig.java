89402b44b63a3d66b1f9ed954be2ccb585c7cf44
Prem Kumar <premkumar1989@gmail.com>
2018-03-29 08:56:36 +0530

    allowing api to be accessed without login

--- src/main/java/com/premkumar/todo/config/WebSecurityConfig.java
+++ src/main/java/com/premkumar/todo/config/WebSecurityConfig.java
@@ -28,9 +28,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+		http.authorizeRequests().antMatchers("/api/**").permitAll().anyRequest()
+				.authenticated().and().formLogin().loginPage("/login")
+				.permitAll().and().logout().permitAll();
 	}
 
 	@Override
